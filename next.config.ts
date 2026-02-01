6055cd448a505022865fe6bc3b3ad48da31fdf0f
katehhh25 <kate@alts.law>
2026-02-01 21:24:01 +0000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,5 @@
 import type { NextConfig } from "next";
-import path from "node:path";
-const LOADER = path.resolve(__dirname, 'src/visual-edits/component-tagger-loader.js');
+
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
@@ -20,12 +19,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  turbopack: {
-    rules: {
-      "*.{jsx,tsx}": {
-        loaders: [LOADER]
-      }
-    }
-  }
 };
+
 export default nextConfig;
