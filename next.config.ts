64ed8a0f0893eeaafcfb7c264a1615d7d9095193
katehhh25 <kate@alts.law>
2026-02-01 21:21:32 +0000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 import type { NextConfig } from "next";
 import path from "node:path";
-
 const LOADER = path.resolve(__dirname, 'src/visual-edits/component-tagger-loader.js');
-
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
@@ -16,7 +14,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  outputFileTracingRoot: path.resolve(__dirname, '../../'),
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -31,6 +28,4 @@ const nextConfig: NextConfig = {
     }
   }
 };
-
 export default nextConfig;
-// Orchids restart: 1769538600680
